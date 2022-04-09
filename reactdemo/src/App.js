1efc0595d8331142b301a3f26a3bd44849def127
SumayaShemo <sumayaahmedbd@gmail.com>
2022-04-09 22:04:11 +0600

    Second Commit

--- reactdemo/src/App.js
+++ reactdemo/src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Expenses from './component/Expenses/Expenses';
 
-function App() {
+const App=()=> {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Home Loan Bill',
+      amount: 5000,
+      date: new Date(2022, 4, 14),
+    },
+    { id: 'e2', title: 'Eletricity Bill', amount: 799.49, date: new Date(2022, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2022, 2, 2),
+    },
+    {
+      id: 'e4',
+      title: 'Education Loan',
+      amount: 1000,
+      date: new Date(2022, 4, 5),
+    },
+  ];
+ 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Start With React </p>
+        <Expenses items={expenses} />
+        
       </header>
     </div>
   );
